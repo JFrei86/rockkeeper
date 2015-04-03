3c3e9fd84eb15aeea163c497ea4a54708bd96a01
Jesse Freitas <freitas.jesse@gmail.com>
2015-04-02 20:59:49 -0400

    Splashscreen started

---
+++ RocKeeper/app/src/main/src/transcend/rockeeper/activities/Splash.java
@@ -0,0 +1,33 @@
+package transcend.rockeeper.activities;
+
+import transcend.rockeeper.R;
+import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Menu;
+
+public class Splash extends ActionBarActivity {
+
+	/** Duration of wait **/
+    private final int SPLASH_DISPLAY_LENGTH = 1000;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        setContentView(R.layout.activity_splash);
+
+        /* New Handler to start the Menu-Activity 
+         * and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(Splash.this,Menu.class);
+                Splash.this.startActivity(mainIntent);
+                Splash.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
