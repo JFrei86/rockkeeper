cd601e6cd765d0af5acec9ae3e14768405a2de9f
Alex Miropolsky <miropa@rpi.edu>
2015-04-27 12:35:59 -0400

    asdf

--- RocKeeper/app/src/main/java/transcend/rockeeper/data/GoalContract.java
+++ RocKeeper/app/src/main/java/transcend/rockeeper/data/GoalContract.java
@@ -1,3 +1,12 @@
+// I want to get XXXXX points all time. I'm currently at XXXXXX points.
+// I want to atttempt XX routes and one day. I want to complete at least XX of them.
+// I want to climb a VX by XX/XX/XXXX .
+
+// Alex start start XML - get us an idea of what we're looking it - visualize it
+// Jesse think about how the skelleton framework (null values in the database, column with climb/attempt/get...)
+// Dropdown to choose question format and interchangeable verbs
+// column to indicate type (difficulty, points, completed). THe columsn will be null except for column indicated by thte type.
+
 package transcend.rockeeper.data;
 
 public class GoalContract extends Contract {
