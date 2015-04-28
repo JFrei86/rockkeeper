f7eef9f05321a78545cc6e9b4d537868402e81ae
Jesse Freitas <freitas.jesse@gmail.com>
2015-04-27 21:11:54 -0400

    Formatting, Comments

--- RocKeeper/app/src/main/java/transcend/rockeeper/data/Contract.java
+++ RocKeeper/app/src/main/java/transcend/rockeeper/data/Contract.java
@@ -11,116 +11,243 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.provider.BaseColumns;
 
-public abstract class Contract implements BaseColumns{
+/**
+ * A base contract containing helper functions for interacting with the database
+ * 
+ * @author Team Transcend
+ */
+public abstract class Contract implements BaseColumns {
 
 	public static final String TEXT = "TEXT";
 	public static final String INT = "INT";
 	public static final String NUM = "NUM";
 	public static final String REAL = "REAL";
-	
+
 	public int id = 0;
-	
+
 	protected Map<String, String> colTypes = new TreeMap<String, String>();
-	
+
 	public static final String CREATED_ON = "created_on";
 	public static final String MODIFIED_ON = "modified_on";
-	
+
+	/**
+	 * @return the table name of the contract
+	 */
 	public abstract String tableName();
-	
-	public Contract(){
+
+	/**
+	 * Default constructor for all database objects
+	 */
+	public Contract() {
 		colTypes.put(CREATED_ON, TEXT);
 		colTypes.put(MODIFIED_ON, TEXT);
 	}
-	
-	//Create table SQL Command String
+
+	// Create table SQL Command String
+	/**
+	 * @return a SQL executable string for creating a table in SQLite
+	 */
 	public String createTable() {
-		String command = "CREATE TABLE " + tableName() + " (" +
-		Contract._ID + " INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL";
+		String command = "CREATE TABLE " + tableName() + " (" + Contract._ID
+				+ " INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL";
 		Iterator<String> i = colTypes.keySet().iterator();
-		while(i.hasNext()){
+		while (i.hasNext()) {
 			String key = i.next();
 			command += ", " + key + " " + colTypes.get(key);
 		}
 		return command + "); ";
 	}
-	
-	//Drop table SQL Command String
+
+	// Drop table SQL Command String
+	/**
+	 * @return a SQL executable string for dropping a table in SQLite
+	 */
 	public String dropTable() {
 		return "DROP TABLE IF EXISTS " + tableName() + ";";
 	}
-	
-	//Put a new document in the database
-	public long insert(Unit d, SQLiteDatabase db){
+
+	// Put a new document in the database
+	/**
+	 * @param d
+	 *            A Unit of data containing keys to insert into the table
+	 * @param db
+	 *            The database to insert into. Database must be read/write
+	 * @return the ID of the Unit created. Unit will be modified to contain the
+	 *         new id as well.
+	 */
+	public long insert(Unit d, SQLiteDatabase db) {
 		d.put(CREATED_ON, new Date().toString());
 		d.put(MODIFIED_ON, new Date().toString());
 		ContentValues values = new ContentValues();
 		Iterator<String> i = d.keySet().iterator();
-		while(i.hasNext()){
+		while (i.hasNext()) {
 			String key = i.next();
-			if(colTypes.containsKey(key)){
-				if(colTypes.get(key) == INT){
+			if (colTypes.containsKey(key)) {
+				if (colTypes.get(key) == INT) {
 					values.put(key, Long.parseLong(d.get(key)));
 				}
-				if(colTypes.get(key) == NUM){
+				if (colTypes.get(key) == NUM) {
 					values.put(key, Double.parseDouble(d.get(key)));
 				}
-				if(colTypes.get(key) == TEXT){
+				if (colTypes.get(key) == TEXT) {
 					values.put(key, d.get(key));
 				}
 			}
 		}
-		long id =  db.insert(tableName(), null, values);
+		long id = db.insert(tableName(), null, values);
 		d.put(_ID, id);
 		return id;
 	}
-	
-	public Cursor query(String[] retrieve, String where, String[] args, String sortBy, boolean descending, Integer limit, SQLiteDatabase db){
-		if(limit != null)
-			return db.query(tableName(), retrieve, where, args, null, null, sortBy + ((descending)?" DESC":" ASC"), limit.toString());
+
+	/**
+	 * @param retrieve
+	 *            An array of columns to retrieve from the database
+	 * @param where
+	 *            A SQL where clause that the query will use to look up values
+	 * @param args
+	 *            Query arguments
+	 * @param sortBy
+	 *            A SQL column string to sort by
+	 * @param descending
+	 *            A boolean to determine the sort direction (descending = true,
+	 *            ascending = false)
+	 * @param limit
+	 *            A limit value (null is no limit clause)
+	 * @param db
+	 *            A read only or read/write database reference
+	 * @return A cursor containing the results of the query
+	 */
+	public Cursor query(String[] retrieve, String where, String[] args,
+			String sortBy, boolean descending, Integer limit, SQLiteDatabase db) {
+		if (limit != null)
+			return db.query(tableName(), retrieve, where, args, null, null,
+					sortBy + ((descending) ? " DESC" : " ASC"),
+					limit.toString());
 		else
-			return db.query(tableName(), retrieve, where, args, null, null, sortBy + ((descending)?" DESC":" ASC"), null);
+			return db.query(tableName(), retrieve, where, args, null, null,
+					sortBy + ((descending) ? " DESC" : " ASC"), null);
 	}
-	
-	public void delete(String where, String[] args, SQLiteDatabase db){
+
+	/**
+	 * @param where
+	 *            A SQL where clause that the query will use to look up values
+	 * @param args
+	 *            Query arguments
+	 * @param db
+	 *            A read/write database reference
+	 */
+	public void delete(String where, String[] args, SQLiteDatabase db) {
 		db.delete(tableName(), where, args);
 	}
-	
-	public long update(Unit d, String where, String[] args, SQLiteDatabase db){
+
+	/**
+	 * @param d
+	 * @param where
+	 *            A SQL where clause that the query will use to look up values
+	 * @param args
+	 *            Query arguments
+	 * @param db
+	 *            A read/write database reference
+	 * @return the number of documents in the database that were updated
+	 */
+	public long update(Unit d, String where, String[] args, SQLiteDatabase db) {
 		d.put(Contract.MODIFIED_ON, new Date().toString());
 		ContentValues values = new ContentValues();
 		Iterator<String> i = d.keySet().iterator();
-		while(i.hasNext()){
+		while (i.hasNext()) {
 			String key = i.next();
-			if(colTypes.containsKey(key)){
-				if(colTypes.get(key) == INT){
+			if (colTypes.containsKey(key)) {
+				if (colTypes.get(key) == INT) {
 					values.put(key, Long.parseLong(d.get(key)));
 				}
-				if(colTypes.get(key) == NUM){
+				if (colTypes.get(key) == NUM) {
 					values.put(key, Double.parseDouble(d.get(key)));
 				}
-				if(colTypes.get(key) == TEXT){
+				if (colTypes.get(key) == TEXT) {
 					values.put(key, d.get(key));
 				}
 			}
 		}
 		return db.update(tableName(), values, where, args);
 	}
-	
-	public class Unit{
+
+	/**
+	 * A model for inserting keys into the database
+	 * 
+	 * @author Team Transcend
+	 *
+	 */
+	public class Unit {
 		public TreeMap<String, String> data = new TreeMap<String, String>();
-		public Set<String> keySet() { return data.keySet(); }
-		public String get(String key) { return data.get(key); }
-		public void put(String col, String val) { data.put(col, val); }
-		public void put(String col, Integer val) { put(col, "" + val); }
-		public void put(String col, Long val) { put(col, "" + val); }
-		public void put(String col, Double val) { put(col, "" + val); }
-		public void remove(String col) { data.remove(col); }
-		public String toString(){return data.toString();}
-		public boolean equals(Object a){ 
-			if(a != null && ((Unit) a).get(_ID) != null)
-				return ((Unit)a).get(_ID).equals(this.get(_ID));
-			else return false;
+
+		/**
+		 * @return The set of keys in the unit currently
+		 */
+		public Set<String> keySet() {
+			return data.keySet();
 		}
-		public Unit(){}
+
+		/**
+		 * @param key
+		 *            the column to retrieve from the unit
+		 * @return The value from the unit corresponding to the key
+		 */
+		public String get(String key) {
+			return data.get(key);
+		}
+
+		/**
+		 * @param col
+		 *            the column to insert a value
+		 * @param val
+		 *            the value to insert into the unit
+		 */
+		public void put(String col, String val) {
+			data.put(col, val);
+		}
+
+		public void put(String col, Integer val) {
+			put(col, "" + val);
+		}
+
+		public void put(String col, Long val) {
+			put(col, "" + val);
+		}
+
+		public void put(String col, Double val) {
+			put(col, "" + val);
+		}
+
+		/**
+		 * @param col
+		 *            remove a value from the unit if it exists
+		 */
+		public void remove(String col) {
+			data.remove(col);
+		}
+
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see java.lang.Object#toString()
+		 */
+		public String toString() {
+			return data.toString();
+		}
+
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see java.lang.Object#equals(java.lang.Object)
+		 */
+		public boolean equals(Object a) {
+			if (a != null && ((Unit) a).get(_ID) != null)
+				return ((Unit) a).get(_ID).equals(this.get(_ID));
+			else
+				return false;
+		}
+
+		public Unit() {
+		}
 	}
 }
--- RocKeeper/app/src/main/java/transcend/rockeeper/data/RouteContract.java
+++ RocKeeper/app/src/main/java/transcend/rockeeper/data/RouteContract.java
@@ -5,17 +5,16 @@ import android.provider.BaseColumns;
 
 public class RouteContract extends Contract implements BaseColumns {
 
-	//Define Columns
+	// Define Columns
 	public static String DIFFICULTY = "difficulty";
 	public static String NUM_ATTEMPTS = "num_attempts";
 	public static String COLOR = "color";
 	public static String NAME = "name";
 	public static String LOCATION = "location";
 	public static String COMPLETED = "complete";
-    public static String POINTS = "points";
+	public static String POINTS = "points";
 
-	
-	//Define Column Types
+	// Define Column Types
 	public RouteContract() {
 		super();
 		colTypes.put(DIFFICULTY, TEXT);
@@ -24,21 +23,27 @@ public class RouteContract extends Contract implements BaseColumns {
 		colTypes.put(COLOR, INT);
 		colTypes.put(NAME, TEXT);
 		colTypes.put(COMPLETED, INT);
-        colTypes.put(POINTS, INT);
+		colTypes.put(POINTS, INT);
 	}
-	
-	//Table name for contract
-	@Override
-	public String tableName() {return "routes";}
 
-	public Route build(String difficulty, int attempts, long loc_id, int color, String name, int completed, int points){
-		return this.new Route(null, difficulty, attempts, loc_id, color, name, completed, points);
+	// Table name for contract
+	@Override
+	public String tableName() {
+		return "routes";
 	}
-	
-	//Default values for schema
-	public class Route extends Unit{
-		public Route(Long _id, String difficulty, int attempts, long loc_id, int color, String name, int completed, int points){
-			if(_id != null)
+
+	// A factory function for creating a Route object
+	public Route build(String difficulty, int attempts, long loc_id, int color,
+			String name, int completed, int points) {
+		return this.new Route(null, difficulty, attempts, loc_id, color, name,
+				completed, points);
+	}
+
+	// Default values for schema
+	public class Route extends Unit {
+		public Route(Long _id, String difficulty, int attempts, long loc_id,
+				int color, String name, int completed, int points) {
+			if (_id != null)
 				put(_ID, _id);
 			put(DIFFICULTY, difficulty);
 			put(NUM_ATTEMPTS, attempts);
@@ -46,19 +51,19 @@ public class RouteContract extends Contract implements BaseColumns {
 			put(COLOR, color);
 			put(NAME, name);
 			put(COMPLETED, completed);
-            put(POINTS, points);
+			put(POINTS, points);
 		}
 	}
 
+	// Factory function that builds a Route from a cursor pointing to a result
+	// from a query
 	public Route build(Cursor c) {
-		return this.new Route(
-				c.getLong(c.getColumnIndex(_ID)),
-				c.getString(c.getColumnIndex(DIFFICULTY)),
-				c.getInt(c.getColumnIndex(NUM_ATTEMPTS)),
-				c.getLong(c.getColumnIndex(LOCATION)),
-				c.getInt(c.getColumnIndex(COLOR)),
-				c.getString(c.getColumnIndex(NAME)),
-				c.getInt(c.getColumnIndex(COMPLETED)),
-                c.getInt(c.getColumnIndex(POINTS)));
+		return this.new Route(c.getLong(c.getColumnIndex(_ID)), c.getString(c
+				.getColumnIndex(DIFFICULTY)), c.getInt(c
+				.getColumnIndex(NUM_ATTEMPTS)), c.getLong(c
+				.getColumnIndex(LOCATION)), c.getInt(c.getColumnIndex(COLOR)),
+				c.getString(c.getColumnIndex(NAME)), c.getInt(c
+						.getColumnIndex(COMPLETED)), c.getInt(c
+						.getColumnIndex(POINTS)));
 	}
 }
